7c470ec0d4e42ddb83b304e6fc8a02349fd68440
bstachmann <egal>
2016-02-14 18:09:11 +0100

    Refactoring: Fingerprint in eine Liste verpacken.

--- src/de/bst/kata/bloom/Filter.java
+++ src/de/bst/kata/bloom/Filter.java
@@ -1,9 +1,11 @@
 package de.bst.kata.bloom;
 
 import static java.lang.Math.*;
+import static java.util.Arrays.*;
 
 import java.io.Serializable;
 import java.util.BitSet;
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class Filter implements Serializable {
@@ -13,11 +15,15 @@ public class Filter implements Serializable {
 	private BitSet userBits = new BitSet(size);
 
 	public void add(String id) {
-		userBits.set(fingerprint(id));
+		userBits.set(fingerprints(id).get(0));
 	}
 
 	public boolean contains(String id) {
-		return userBits.get(fingerprint(id));
+		return userBits.get(fingerprints(id).get(0));
+	}
+
+	private List<Integer> fingerprints(String id) {
+		return asList(fingerprint(id));
 	}
 
 	private Integer fingerprint(String id) {
