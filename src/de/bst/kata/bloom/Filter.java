cd6b12cb13bc8ad37580090b34fb2584bcc69d43
bstachmann <egal>
2016-02-14 18:15:34 +0100

    (5) Mehrere Bits pro User

--- src/de/bst/kata/bloom/Filter.java
+++ src/de/bst/kata/bloom/Filter.java
@@ -10,20 +10,22 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class Filter implements Serializable {
 
-	private int size = 1200000;
+	private int size = 600000;
 
 	private BitSet userBits = new BitSet(size);
 
 	public void add(String id) {
 		userBits.set(fingerprints(id).get(0));
+		userBits.set(fingerprints(id).get(1));
 	}
 
 	public boolean contains(String id) {
-		return userBits.get(fingerprints(id).get(0));
+		return userBits.get(fingerprints(id).get(0)) //
+				&& userBits.get(fingerprints(id).get(1));
 	}
 
 	private List<Integer> fingerprints(String id) {
-		return asList(fingerprint(id));
+		return asList(fingerprint(id), fingerprint(id + id));
 	}
 
 	private Integer fingerprint(String id) {
